3e32413e4dfd4619cd371f348ea262c6f384f31c
admin <tablabstechnologies@gmail.com>
2026-02-02 18:27:40 +0530

    added app localization file

---
+++ lib/l10n/app_localizations_ml.dart
@@ -0,0 +1,337 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Malayalam (`ml`).
+class AppLocalizationsMl extends AppLocalizations {
+  AppLocalizationsMl([String locale = 'ml']) : super(locale);
+
+  @override
+  String get appTitle => 'ഫില്മിറ്റെൽ';
+
+  @override
+  String get skip => 'അടുക്കുക';
+
+  @override
+  String get next => 'അടുത്തത്';
+
+  @override
+  String get getStarted => 'തുടങ്ങി';
+
+  @override
+  String get login => 'ലോഗിൻ';
+
+  @override
+  String get mobileNumber => 'മൊബൈൽ നമ്പർ';
+
+  @override
+  String get enterMobileNumber => 'മൊബൈൽ നമ്പർ നൽകുക';
+
+  @override
+  String get sendOtp => 'ഒടിപി അയയ്ക്കുക';
+
+  @override
+  String get enterOtp => 'ഒടിപി നൽകുക';
+
+  @override
+  String get verifyOtp => 'ഒടിപി സ്ഥിരീകരിക്കുക';
+
+  @override
+  String get otpSentSuccessfully => 'ഒടിപി വിജയകരമായി അയച്ചു';
+
+  @override
+  String get loginSuccessfully => 'വിജയകരമായി ലോഗിൻ ചെയ്തു';
+
+  @override
+  String get home => 'ഹോം';
+
+  @override
+  String get homeTitle => 'അന്വേഷിക്കുക, കാണുക, അനുഭവിക്കുക...';
+
+  @override
+  String get movie => 'ചലച്ചിത്രം';
+
+  @override
+  String get series => 'സീരീസ്';
+
+  @override
+  String get enterDetails => 'വിശദാംശങ്ങൾ നൽകുക';
+
+  @override
+  String get birthDate => 'ജനന തീയതി';
+
+  @override
+  String get enterBirthDate => 'ജനന തീയതി നൽകുക';
+
+  @override
+  String get firstName => 'പേരിന്റെ ആദ്യഭാഗം';
+
+  @override
+  String get enterFirstName => 'പേരിന്റെ ആദ്യഭാഗം നൽകുക';
+
+  @override
+  String get lastName => 'പേരിന്റെ അവസാനഭാഗം';
+
+  @override
+  String get enterLastName => 'പേരിന്റെ അവസാനഭാഗം നൽകുക';
+
+  @override
+  String get email => 'ഇമെയിൽ';
+
+  @override
+  String get enterEmail => 'സരിയായ ഇമെയിൽ നൽകുക';
+
+  @override
+  String get referralCodeOptional => 'റഫറൽ കോഡ് (ഐച്ഛികം)';
+
+  @override
+  String get enterReferralCode => 'റഫറൽ കോഡ് നൽകുക';
+
+  @override
+  String get profileUpdatedSuccessfully =>
+      'പ്രൊഫൈൽ വിജയകരമായി അപ്ഡേറ്റ് ചെയ്തു!';
+
+  @override
+  String get save => 'സംരക്ഷിക്കുക';
+
+  @override
+  String get enterLocationDetails => 'ലൊക്കേഷൻ വിശദാംശങ്ങൾ നൽകുക';
+
+  @override
+  String get country => 'രാജ്യം';
+
+  @override
+  String get enterCountry => 'സരിയായ രാജ്യം നൽകുക';
+
+  @override
+  String get state => 'സംസ്ഥാനം';
+
+  @override
+  String get enterState => 'സരിയായ സംസ്ഥാനം നൽകുക';
+
+  @override
+  String get district => 'ജില്ല';
+
+  @override
+  String get enterDistrict => 'സരിയായ ജില്ല നൽകുക';
+
+  @override
+  String get taluka => 'താലൂക്ക്';
+
+  @override
+  String get enterTaluka => 'സരിയായ താലൂക്ക് നൽകുക';
+
+  @override
+  String get search => 'തിരയുക';
+
+  @override
+  String get searchContent => 'ഉള്ളടക്കം തിരയുക';
+
+  @override
+  String get filter => 'ഫിൽറ്റർ';
+
+  @override
+  String get filterOptions => 'ഫിൽറ്റർ ഓപ്ഷനുകൾ';
+
+  @override
+  String get genre => 'തരം';
+
+  @override
+  String get selectGenre => 'തരം തിരഞ്ഞെടുക്കുക';
+
+  @override
+  String get language => 'ഭാഷ';
+
+  @override
+  String get selectLanguage => 'ഭാഷ തിരഞ്ഞെടുക്കുക';
+
+  @override
+  String get minimumRating => 'കുറഞ്ഞത് റേറ്റിംഗ്';
+
+  @override
+  String get selectRating => 'റേറ്റിംഗ് തിരഞ്ഞെടുക്കുക';
+
+  @override
+  String get clearFilter => 'ഫിൽറ്റർ ഇല്ലാതാക്കുക';
+
+  @override
+  String get noContentFound => 'ഉള്ളടക്കം കണ്ടെത്തിയില്ല';
+
+  @override
+  String get watchlist => 'വാച്ച്‌ലിസ്റ്റ്';
+
+  @override
+  String get noContentAvailable => 'ലഭ്യമായ ഉള്ളടക്കം ഇല്ല';
+
+  @override
+  String get downloads => 'ഡൗൺലോഡുകൾ';
+
+  @override
+  String get upcoming => 'വരാനിരിക്കുന്ന';
+
+  @override
+  String get notifyMe => 'എനിക്ക് അറിയിക്കുക';
+
+  @override
+  String get profile => 'പ്രൊഫൈൽ';
+
+  @override
+  String get editProfile => 'പ്രൊഫൈൽ എഡിറ്റ് ചെയ്യുക';
+
+  @override
+  String get wallet => 'വാലറ്റ്';
+
+  @override
+  String get availableBalance => 'ലഭ്യമായ ബാലൻസ്';
+
+  @override
+  String get readyToStreamYourfavorites =>
+      'നിങ്ങളുടെ ഇഷ്ടങ്ങളൊക്കെ സ്റ്റ്രീം ചെയ്യാൻ തയ്യാറാണ്';
+
+  @override
+  String get addBalance => 'ബാലൻസ് ചേർക്കുക';
+
+  @override
+  String get transactionHistory => ' ഇടപാട് ചരിത്രം';
+
+  @override
+  String get selectDate => 'തീയതി തിരഞ്ഞെടുക്കുക';
+
+  @override
+  String get noTransactionsFound => 'ഇടപാടുകൾ ഒന്നും കണ്ടെത്തിയില്ല';
+
+  @override
+  String get rechargeWallet => 'വാലറ്റ് റീചാർജ് ചെയ്യുക';
+
+  @override
+  String get enterAmountMin10 => 'തുക നൽകുക (കുറഞ്ഞത് ₹10)';
+
+  @override
+  String get cancel => 'റദ്ദാക്കുക';
+
+  @override
+  String get minimumRechargeAmountIs10 => 'കുറഞ്ഞ റീചാർജ് തുക ₹10 ആണ്';
+
+  @override
+  String get walletRechargedWith => 'വാലറ്റ് റീചാർജ് ചെയ്തു';
+
+  @override
+  String get recharge => 'റീചാർജ്';
+
+  @override
+  String get help => 'സഹായം';
+
+  @override
+  String get helpAndSupport => 'സഹായവും പിന്തുണയും';
+
+  @override
+  String get enterYourQuery => 'താങ്കളുടെ ചോദ്യങ്ങൾ നൽകുക';
+
+  @override
+  String get attachImage => 'ചിത്രം ചേർക്കുക';
+
+  @override
+  String get submit => 'സമർപ്പിക്കുക';
+
+  @override
+  String get sent => 'അയച്ചു';
+
+  @override
+  String get translate => 'പരിഭാഷ ചെയ്യുക';
+
+  @override
+  String get notification => 'അറിയിപ്പുകൾ';
+
+  @override
+  String get changeLanguage => 'ഭാഷ മാറ്റുക';
+
+  @override
+  String get selectPreferredLanguage =>
+      'താങ്കളുടെ ഇഷ്ടപ്പെട്ട ഭാഷ തിരഞ്ഞെടുക്കുക';
+
+  @override
+  String get director => 'സംവിധായകൻ';
+
+  @override
+  String get cast => 'നടൻമാർ';
+
+  @override
+  String get genres => 'തരങ്ങൾ';
+
+  @override
+  String get runtime => 'നേരം';
+
+  @override
+  String get releaseDate => 'റിലീസ് തീയതി';
+
+  @override
+  String get languages => 'ഭാഷകൾ';
+
+  @override
+  String get rating => 'റേറ്റിംഗ്';
+
+  @override
+  String get audioFormat => 'ഓഡിയോ ഫോർമാറ്റുകൾ';
+
+  @override
+  String get subtitle => 'അടിക്കുറിപ്പ്';
+
+  @override
+  String get ageRating => 'പ്രായപരിധി റേറ്റിംഗ്';
+
+  @override
+  String get rent => 'വാടകയ്ക്ക്';
+
+  @override
+  String get description => 'വിവരണം';
+
+  @override
+  String get watchTrailer => 'ട്രെയ്‌ലർ കാണുക';
+
+  @override
+  String get watchMovie => 'ചലച്ചിത്രം കാണുക';
+
+  @override
+  String get watchSeries => 'സീരീസ് കാണുക';
+
+  @override
+  String get mediaHouse => 'മീഡിയാ ഹൗസ്';
+
+  @override
+  String get rentDuration => 'വാടക കാലാവധി';
+
+  @override
+  String get price => 'വില';
+
+  @override
+  String get myCredits => 'എന്റെ ക്രെഡിറ്റുകൾ';
+
+  @override
+  String get gifts => 'സമ്മാനങ്ങൾ';
+
+  @override
+  String get giftedMovies => 'സമ്മാനമായി ലഭിച്ച സിനിമകൾ';
+
+  @override
+  String get claimGiftCard => 'ഗിഫ്റ്റ് കാർഡ് ക്ലെയിം ചെയ്യുക';
+
+  @override
+  String get features => 'സവിശേഷതകൾ';
+
+  @override
+  String get feedbackAndInformation => 'പ്രതികരണവും വിവരങ്ങളും';
+
+  @override
+  String get termsPoliciesLiscenses => 'നിബന്ധനകൾ, നയങ്ങൾ & ലൈസൻസുകൾ';
+
+  @override
+  String get aboutFilmytell => 'ഫില്മിടെൽ കുറിച്ച്';
+
+  @override
+  String get rateUs => 'ഞങ്ങളെ റേറ്റ് ചെയ്യുക';
+
+  @override
+  String get logout => 'ലോഗൗട്ട്';
+}
---
+++ lib/l10n/app_localizations_ta.dart
@@ -0,0 +1,336 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Tamil (`ta`).
+class AppLocalizationsTa extends AppLocalizations {
+  AppLocalizationsTa([String locale = 'ta']) : super(locale);
+
+  @override
+  String get appTitle => 'Filmytell';
+
+  @override
+  String get skip => 'தவிர்க்கவும்';
+
+  @override
+  String get next => 'அடுத்தது';
+
+  @override
+  String get getStarted => 'தொடங்கு';
+
+  @override
+  String get login => 'உள்நுழை';
+
+  @override
+  String get mobileNumber => 'மொபைல் எண்';
+
+  @override
+  String get enterMobileNumber => 'மொபைல் எண்ணை உள்ளிடவும்';
+
+  @override
+  String get sendOtp => 'OTP அனுப்பு';
+
+  @override
+  String get enterOtp => 'OTP ஐ உள்ளிடவும்';
+
+  @override
+  String get verifyOtp => 'OTP சரிபார்க்கவும்';
+
+  @override
+  String get otpSentSuccessfully => 'OTP வெற்றிகரமாக அனுப்பப்பட்டது';
+
+  @override
+  String get loginSuccessfully => 'வெற்றிகரமாக உள்நுழைந்தீர்கள்';
+
+  @override
+  String get home => 'முகப்பு';
+
+  @override
+  String get homeTitle => 'கண்டறி, பாரு, அனுபவிக்க...';
+
+  @override
+  String get movie => 'திரைப்படம்';
+
+  @override
+  String get series => 'தொடர்கள்';
+
+  @override
+  String get enterDetails => 'விவரங்களை உள்ளிடவும்';
+
+  @override
+  String get birthDate => 'பிறந்த தேதி';
+
+  @override
+  String get enterBirthDate => 'பிறந்த தேதியை உள்ளிடவும்';
+
+  @override
+  String get firstName => 'முதல் பெயர்';
+
+  @override
+  String get enterFirstName => 'முதல் பெயரை உள்ளிடவும்';
+
+  @override
+  String get lastName => 'கடைசி பெயர்';
+
+  @override
+  String get enterLastName => 'கடைசி பெயரை உள்ளிடவும்';
+
+  @override
+  String get email => 'மின்னஞ்சல்';
+
+  @override
+  String get enterEmail => 'சரியான மின்னஞ்சலை உள்ளிடவும்';
+
+  @override
+  String get referralCodeOptional => 'பரிந்துரை குறியீடு (விருப்பம்)';
+
+  @override
+  String get enterReferralCode => 'பரிந்துரை குறியீட்டை உள்ளிடவும்';
+
+  @override
+  String get profileUpdatedSuccessfully =>
+      'சுயவிவரம் வெற்றிகரமாக புதுப்பிக்கப்பட்டது!';
+
+  @override
+  String get save => 'சேமிக்கவும்';
+
+  @override
+  String get enterLocationDetails => 'இருப்பிட விவரங்களை உள்ளிடவும்';
+
+  @override
+  String get country => 'நாடு';
+
+  @override
+  String get enterCountry => 'சரியான நாட்டை உள்ளிடவும்';
+
+  @override
+  String get state => 'மாநிலம்';
+
+  @override
+  String get enterState => 'சரியான மாநிலத்தை உள்ளிடவும்';
+
+  @override
+  String get district => 'மாவட்டம்';
+
+  @override
+  String get enterDistrict => 'சரியான மாவட்டத்தை உள்ளிடவும்';
+
+  @override
+  String get taluka => 'தாலுகா';
+
+  @override
+  String get enterTaluka => 'சரியான தாலுகாவை உள்ளிடவும்';
+
+  @override
+  String get search => 'தேடல்';
+
+  @override
+  String get searchContent => 'உள்ளடக்கத்தைத் தேடவும்';
+
+  @override
+  String get filter => 'வடிகட்டி';
+
+  @override
+  String get filterOptions => 'வடிகட்டும் விருப்பங்கள்';
+
+  @override
+  String get genre => 'பண்பாடு';
+
+  @override
+  String get selectGenre => 'பண்பாட்டைத் தேர்ந்தெடுக்கவும்';
+
+  @override
+  String get language => 'மொழி';
+
+  @override
+  String get selectLanguage => 'மொழியைத் தேர்ந்தெடுக்கவும்';
+
+  @override
+  String get minimumRating => 'குறைந்தபட்ச மதிப்பீடு';
+
+  @override
+  String get selectRating => 'மதிப்பீட்டைத் தேர்ந்தெடுக்கவும்';
+
+  @override
+  String get clearFilter => 'வடிகட்டியை அகற்று';
+
+  @override
+  String get noContentFound => 'உள்ளடக்கம் காணப்படவில்லை';
+
+  @override
+  String get watchlist => 'பார்ப்புப் பட்டியல்';
+
+  @override
+  String get noContentAvailable => 'உள்ளடக்கம் கிடைக்கவில்லை';
+
+  @override
+  String get downloads => 'பதிவிறக்கங்கள்';
+
+  @override
+  String get upcoming => 'விரைவில்';
+
+  @override
+  String get notifyMe => 'எனக்கு அறிவிக்கவும்';
+
+  @override
+  String get profile => 'சுயவிவரம்';
+
+  @override
+  String get editProfile => 'சுயவிவரத்தைத் திருத்தவும்';
+
+  @override
+  String get wallet => 'பணப்பை';
+
+  @override
+  String get availableBalance => 'கிடைக்கும் இருப்பு';
+
+  @override
+  String get readyToStreamYourfavorites =>
+      'உங்கள் விருப்பங்களை ஸ்ட்ரீம் செய்ய தயாராக உள்ளீர்கள்';
+
+  @override
+  String get addBalance => 'இருப்பைச் சேர்';
+
+  @override
+  String get transactionHistory => 'பரிவர்த்தனை வரலாறு';
+
+  @override
+  String get selectDate => 'தேதியைத் தேர்ந்தெடுக்கவும்';
+
+  @override
+  String get noTransactionsFound => 'பரிவர்த்தனைகள் எதுவும் கிடைக்கவில்லை';
+
+  @override
+  String get rechargeWallet => 'பணப்பையை ரீசார்ஜ் செய்யவும்';
+
+  @override
+  String get enterAmountMin10 => 'தொகையை உள்ளிடவும் (குறைந்தபட்சம் ₹10)';
+
+  @override
+  String get cancel => 'ரத்து செய்';
+
+  @override
+  String get minimumRechargeAmountIs10 => 'குறைந்தபட்ச ரீசார்ஜ் தொகை ₹10';
+
+  @override
+  String get walletRechargedWith => 'பணப்பை ரீசார்ஜ் செய்யப்பட்டது';
+
+  @override
+  String get recharge => 'ரீசார்ஜ்';
+
+  @override
+  String get help => 'உதவி';
+
+  @override
+  String get helpAndSupport => 'உதவி மற்றும் ஆதரவு';
+
+  @override
+  String get enterYourQuery => 'உங்கள் கேள்வியை உள்ளிடவும்';
+
+  @override
+  String get attachImage => 'படத்தை இணைக்கவும்';
+
+  @override
+  String get submit => 'சமர்ப்பிக்கவும்';
+
+  @override
+  String get sent => 'அனுப்பப்பட்டது';
+
+  @override
+  String get translate => 'மொழிபெயர்க்கவும்';
+
+  @override
+  String get notification => 'அறிவிப்பு';
+
+  @override
+  String get changeLanguage => 'மொழியை மாற்றவும்';
+
+  @override
+  String get selectPreferredLanguage => 'விருப்பமான மொழியைத் தேர்ந்தெடுக்கவும்';
+
+  @override
+  String get director => 'இயக்குனர்';
+
+  @override
+  String get cast => 'நடிக்கின்றோர்';
+
+  @override
+  String get genres => 'பண்புகள்';
+
+  @override
+  String get runtime => 'நேரம்';
+
+  @override
+  String get releaseDate => 'வெளியீட்டு தேதி';
+
+  @override
+  String get languages => 'மொழிகள்';
+
+  @override
+  String get rating => 'மதிப்பீடு';
+
+  @override
+  String get audioFormat => 'ஆடியோ வடிவங்கள்';
+
+  @override
+  String get subtitle => 'உபதலைப்புகள்';
+
+  @override
+  String get ageRating => 'வயது மதிப்பீடு';
+
+  @override
+  String get rent => 'வாடகை';
+
+  @override
+  String get description => 'விவரணம்';
+
+  @override
+  String get watchTrailer => 'டிரைலரை பாருங்கள்';
+
+  @override
+  String get watchMovie => 'திரைப்படத்தை பாருங்கள்';
+
+  @override
+  String get watchSeries => 'தொடர்களை பாருங்கள்';
+
+  @override
+  String get mediaHouse => 'மீடியா ஹவுஸ்';
+
+  @override
+  String get rentDuration => 'வாடகை காலம்';
+
+  @override
+  String get price => 'விலை';
+
+  @override
+  String get myCredits => 'என் கிரெடிட்கள்';
+
+  @override
+  String get gifts => 'பரிசுகள்';
+
+  @override
+  String get giftedMovies => 'பரிசாக பெற்ற திரைப்படங்கள்';
+
+  @override
+  String get claimGiftCard => 'கிஃப்ட் கார்டை கோருங்கள்';
+
+  @override
+  String get features => 'அம்சங்கள்';
+
+  @override
+  String get feedbackAndInformation => 'கருத்து & தகவல்';
+
+  @override
+  String get termsPoliciesLiscenses => 'விதிமுறைகள், கொள்கைகள் & உரிமங்கள்';
+
+  @override
+  String get aboutFilmytell => 'பில்மிடெல் பற்றி';
+
+  @override
+  String get rateUs => 'எங்களை மதிப்பிடுங்கள்';
+
+  @override
+  String get logout => 'வெளியேறு';
+}
