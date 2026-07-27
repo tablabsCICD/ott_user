530751b6f0b8b0c36fb4923bb2512e0046b53573
admin <tablabstechnologies@gmail.com>
2026-07-27 10:34:42 +0530

    added type for playback security

--- lib/app/provider/playMediaProvider.dart
+++ lib/app/provider/playMediaProvider.dart
@@ -1,6 +1,5 @@
 import 'dart:developer';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
 import 'package:ott/app/core/constant/api_constant.dart';
 import 'package:ott/app/core/network/api_helper.dart';
